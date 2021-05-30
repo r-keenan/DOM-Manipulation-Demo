75fd5b6fd87873d04dc2170d4380db4fab76ec27
Ross Keenan <ross.keenan@protonmail.com>
2021-05-30 15:46:08 -0500

    Manipulating the title for a span and then reverting back to the original title name.

--- script.js
+++ script.js
@@ -20,6 +20,23 @@ const divHTML = document.querySelector("div");
 const spanHi = document.querySelector("#hi");
 const spanBye = document.querySelector("#bye");
 
-//Remove elements from the DOM. 
-spanHi.remove(spanHi)
-spanBye.remove(spanBye)
+//Remove elements from the DOM.
+spanHi.remove(spanHi);
+spanBye.remove(spanBye);
+
+//Add back to the DOM
+divHTML.append(spanHi);
+divHTML.append(spanBye);
+
+//You can use class notation instead of using getAttribute
+//console.log(spanHi.getAttribute("id"));
+console.log(spanHi.id);
+console.log(spanHi.title);
+
+//this changes the title of the spanHi tag from "hi" to "sayHello"
+spanHi.setAttribute("title", "sayHello");
+console.log(spanHi.title);
+
+//changing title back to the original
+spanHi.title = "hello";
+console.log(spanHi.title);
